607c6c369afc62eccefb73072408101d319441a2
Phạm Hồ Tấn Đạt <pdat113204@gmail.com>
2026-02-06 19:47:29 +0700

    Remove configuration from DBContext

--- EduVi.Repositories/DBContext/EduViContext.cs
+++ EduVi.Repositories/DBContext/EduViContext.cs
@@ -72,9 +72,7 @@ public partial class EduViContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EduVi;User ID=sa;Password=12345");
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
